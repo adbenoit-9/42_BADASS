ecd165cc9313b5906bd7370189ea3b0450527185
adbenoit-9 <adeline.benoit@hotmail.com>
2022-10-05 16:13:21 +0200

    [CLEAN]

--- P1/build_images.sh
+++ P1/build_images.sh
@@ -2,12 +2,12 @@
 
 ######################################################
 # *** INSTALL GNS3 & DOCKER ***                      #
-# 	- GNS3 :	sudo add-apt-repository ppa:gns3/ppa #
-# 			    sudo apt update                      #
-# 			    sudo apt install gns3-gui            #
+#    - GNS3 :   sudo add-apt-repository ppa:gns3/ppa #
+#               sudo apt update                      #
+#               sudo apt install gns3-gui            #
 #                                                    #
-# 	- DOCKER :  sudo apt install docker.io           #
-# 			    sudo usermod -a -G docker codebrai   #
+#    - DOCKER :  sudo apt install docker.io          #
+#               sudo usermod -a -G docker codebrai   #
 ######################################################
 
 # Construction of Docker images
