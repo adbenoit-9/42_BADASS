279b1b70ebb1e52abb1dc92cfa3e2643056e258b
adbenoit-9 <adeline.benoit@hotmail.com>
2022-10-05 16:11:26 +0200

    [CLEAN]

--- P1/build_images.sh
+++ P1/build_images.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#####################################################
-# *** INSTALL GNS3 & DOCKER ***                     #
-# 	- GNS3 :	sudo add-apt-repository ppa:gns3/ppa  #
-# 			      sudo apt update                       #
-# 			      sudo apt install gns3-gui             #
-#                                                   #
-# 	- DOCKER : 	sudo apt install docker.io          #
-# 			        sudo usermod -a -G docker codebrai  #
-#####################################################
+######################################################
+# *** INSTALL GNS3 & DOCKER ***                      #
+# 	- GNS3 :	sudo add-apt-repository ppa:gns3/ppa #
+# 			    sudo apt update                      #
+# 			    sudo apt install gns3-gui            #
+#                                                    #
+# 	- DOCKER :  sudo apt install docker.io           #
+# 			    sudo usermod -a -G docker codebrai   #
+######################################################
 
 # Construction of Docker images
 sudo docker build -t alpine -f ./alpine .
